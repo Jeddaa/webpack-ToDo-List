6182f2d009ac4d217f68808d972f4bb02876f58d
jeddaa <fathiato@gmail.com>
2023-03-29 10:40:03 +0100

    making the to-do list

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
